437f9811951439dd5abe17d54b9aca485fd21597
Deven Joshi <deven9852@gmail.com>
2021-05-16 19:34:27 +0530

    portrait by def

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:power_tic_tac_toe/game/tic_tac_toe.dart';
 import 'package:power_tic_tac_toe/game_ui/home_screen.dart';
 import 'package:power_tic_tac_toe/utils/constants.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(MyApp());
 }
 
