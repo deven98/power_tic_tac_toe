0c8d0ef7f22011978c4bc2757ef7ca112eb1bf1e
Deven Joshi <deven9852@gmail.com>
2021-05-17 00:01:01 +0530

    added logos for android and ios

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Power Tic-Tac-Toe',
       theme: ThemeData(
           primarySwatch: Colors.blue,
           textTheme: GoogleFonts.aBeeZeeTextTheme()),
